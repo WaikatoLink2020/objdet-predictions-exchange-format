f719f7f4281ef779745e4768ddabb1c838a34dd5
Peter Reutemann <fracpete@gmail.com>
2023-02-17 15:26:35 +1300

    prepared for release

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     install_requires=[
         "wai.json",
     ],
-    version="0.0.1",
+    version="0.0.2",
     author='Corey Sterling',
     author_email='corey.sterling@waikato.ac.nz',
 )
